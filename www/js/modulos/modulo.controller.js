d0f72016806fd04500e40a853f5baa6f8ce3b300
Guilherme <guilherme@qcx.com.br>
2016-09-19 16:00:12 -0300

    ajustes

--- www/js/modulos/modulo.controller.js
+++ www/js/modulos/modulo.controller.js
@@ -58,7 +58,7 @@
       //   // An error occurred. Show a message to the user
       // });
 
-			window.open('http://redacaoperfeita.s3.amazonaws.com/redacoes/3285/enviada/juniormussel.pdf', '_self');
+			window.open('http://redacaoperfeita.s3.amazonaws.com/redacoes/3285/enviada/juniormussel.pdf', '_system', 'location=yes');
     }
 
     function enviarRedacao() {
