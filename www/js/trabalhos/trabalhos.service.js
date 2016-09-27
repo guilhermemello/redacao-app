d175d4a7a7068012c478fcdedf7a26d035257e1f
Guilherme <guilherme@qcx.com.br>
2016-09-27 16:05:50 -0300

    ajustes

--- www/js/trabalhos/trabalhos.service.js
+++ www/js/trabalhos/trabalhos.service.js
@@ -17,14 +17,14 @@
 		return service;
 
 		function getByModuloId(moduloId) {
-			return $http.get('http://localhost:4000/api/v1/trabalhos/' + moduloId + '/por_modulo').then(function(response) {
+			return $http.get('http://api-hmg.us-east-1.elasticbeanstalk.com/api/v1/trabalhos/' + moduloId + '/por_modulo').then(function(response) {
 				trabalhos = response.data;
 				return trabalhos;
 			});
 		};
 
 		function get(trabalhoId) {
-			return $http.get('http://localhost:4000/api/v1/trabalhos/' + trabalhoId).then(function(response) {
+			return $http.get('http://api-hmg.us-east-1.elasticbeanstalk.com/api/v1/trabalhos/' + trabalhoId).then(function(response) {
 				return response.data.trabalho;
 			});
 		};
