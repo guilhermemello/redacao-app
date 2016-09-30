2c656590f167505c0eb19033208e3277fbd06c57
Guilherme Nascimento <guilherme@qcx.com.br>
2016-09-29 22:14:07 -0300

    ajustes

--- www/js/config/config.module.js
+++ www/js/config/config.module.js
@@ -1,2 +1,2 @@
 angular.module('redacao.config',[])
-  .constant('API_ENDPOINT', 'http://localhost:4000/api/v1');
+  .constant('API_ENDPOINT', 'http://api-hmg.us-east-1.elasticbeanstalk.com/api/v1');
